90cda1b35677cf28ca1c92763c4405977decb74c
yurii.kyselev <yurii.kyselev@stryker.com>
2019-04-18 13:16:27 +0300

    Bug fix.

--- lesson3/UnionExtension.cs
+++ lesson3/UnionExtension.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace lesson3
 {
@@ -8,16 +7,35 @@ namespace lesson3
         public static IEnumerable<(string Name, double Latitude, double Longitude)> Union(this IEnumerable<Barcelona> barcelonas,
             IEnumerable<Barcelona> otherBarcelonas)
         {
-            return barcelonas.Union(otherBarcelonas, new BarcelonaComparer())
-                .Select(x => (x.Name, x.Latitude, x.Longitude));
+            var dict = new Dictionary<int, Barcelona>();
+            foreach (var barcelona in barcelonas)
+            {
+                dict.Add(barcelona.Id, barcelona);
+                yield return (barcelona.Name, barcelona.Latitude, barcelona.Longitude);
+            }
+
+            foreach (var otherBarcelona in otherBarcelonas)
+            {
+                if (!dict.ContainsKey(otherBarcelona.Id))
+                {
+                    dict.Add(otherBarcelona.Id, otherBarcelona);
+                    yield return (otherBarcelona.Name, otherBarcelona.Latitude, otherBarcelona.Longitude);
+                }
+            }
         }
 
         public static IEnumerable<(string Name, double Latitude, double Longitude)> UnionAll(this IEnumerable<Barcelona> barcelonas,
             IEnumerable<Barcelona> otherBarcelonas)
         {
-            var list = barcelonas.ToList();
-            list.AddRange(otherBarcelonas);
-            return list.Select(x => (x.Name, x.Latitude, x.Longitude));
+            foreach (var x in barcelonas)
+            {
+                yield return (x.Name, x.Latitude, x.Longitude);
+            }
+
+            foreach (var x in otherBarcelonas)
+            {
+                yield return (x.Name, x.Latitude, x.Longitude);
+            }
         }
     }
 }
